7c7da53fe9c0522aa51b4a81ddcf1879002f4a12
unomic <>
2023-03-29 17:45:42 +0900

    export 연습, product data

---
+++ src/data.js
@@ -0,0 +1,35 @@
+
+// let a = 10;
+// let b = 100;
+// //a를 다른 파일에서 쓰고싶을때 
+
+// export {a,b} ;
+
+let data = [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000,
+      url:"https://codingapple1.github.io/shop/shoes5.jpg"
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000,
+      url:"https://codingapple1.github.io/shop/shoes2.jpg"
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000,
+      url:"https://codingapple1.github.io/shop/shoes3.jpg"
+    }
+
+  ] ;
+
+  export default data;
